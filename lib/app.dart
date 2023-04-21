93e22a7792fb6acf00a00ea61f591eacd4e303e7
Egor-OS <osipoff.egor@icloud.com>
2023-04-21 20:51:52 +0300

    try theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,23 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
 import 'package:training_and_testing/router/app_router.dart';
 import 'package:training_and_testing/theme/theme.dart';
 import '../controllers/auth_controller.dart';
-import '../router/app_router.dart';
 
 class BonusesApp extends StatelessWidget {
   BonusesApp({super.key});
   final GoRouter appRouter =
       RoutesBonusesApp(authController: Get.find<AuthController>()).router;
+  
+  ThemeMode _themeMode = ThemeMode.system;
   final appTheme = BrandThemeData.dark();
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       theme: appTheme,
+      themeMode: _themeMode,
       debugShowCheckedModeBanner: false,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:get/get.dart';
+import 'app.dart';
 import 'constants/generated/codegen_loader.g.dart';
 import 'controllers/auth_controller.dart';
-import 'screens/bonusesApp.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
