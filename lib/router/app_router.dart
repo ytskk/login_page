868ab9b45ba3420074787c47e5f076a5a24f3d16
Egor-OS <osipoff.egor@icloud.com>
2023-05-12 00:37:31 +0300

    #4

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:training_and_testing/controllers/controllers.dart';
 import 'package:training_and_testing/router/app_route_names.dart';
-import 'package:training_and_testing/screens/edit_profile_screen/edit_profile_screen.dart';
-import 'package:training_and_testing/screens/login_screen/login_screen.dart';
 import 'package:training_and_testing/screens/screens.dart';
+
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
 
 class RoutesBonusesApp {
   RoutesBonusesApp({required this.authController});
@@ -13,6 +13,7 @@ class RoutesBonusesApp {
   final AuthController authController;
 
   late final _router = GoRouter(
+    navigatorKey: _rootNavigatorKey,
     initialLocation: '/home',
     // while working on catalog screen, uncomment the line above and comment
     // the line below to set the app initial location.
@@ -23,43 +24,62 @@ class RoutesBonusesApp {
         name: 'login_screen',
         builder: (context, state) => const LogInScreen(),
       ),
-      GoRoute(
-        path: '/home',
-        name: 'home_screen',
-        builder: (context, state) {
-          return const HomeScreen();
-        },
-        redirect: (context, state) {
-          return null;
-        },
+      StatefulShellRoute(
+        branches: [],
+        navigatorContainerBuilder: (
+          BuildContext context,
+          StatefulNavigationShell navigationShell,
+          List<Widget> children,
+        ) =>
+            MainScaffold(navigationShell: navigationShell, children: children),
       ),
-      GoRoute(
-        path: '/edit_profile',
-        name: 'edit_profile',
-        builder: (context, state) => const EditProfileScreen(),
-      ),
-      AppRoute(
-        name: AppRouteNames.catalog,
-        builder: (_, __) => const CatalogScreen(),
-        routes: [
-          AppRoute(
-            name: AppRouteNames.catalogFilters,
-            path: AppRouteNames.catalogFilters,
-            builder: (_, __) => const FiltersScreen(),
-          ),
-        ],
-      ),
-      AppRoute(
-        name: AppRouteNames.cart,
-        builder: (_, __) => const CartScreen(),
-      ),
+      //   ShellRoute(
+      //     builder: (context, state, child) => MainScaffold(child),
+      //     routes: [
+      //       GoRoute(
+      //         path: '/home',
+      //         name: 'home_screen',
+      //         builder: (context, state) {
+      //           return const HomeScreen();
+      //         },
+      //         routes: [
+      //           GoRoute(
+      //             parentNavigatorKey: _rootNavigatorKey,
+      //             path: 'notifications',
+      //             name: 'notification_screen',
+      //             builder: (context, state) => const NotificationScreen(),
+      //           ),
+      //         ],
+      //       ),
+      //       GoRoute(
+      //         path: '/profile',
+      //         name: 'profile_screen',
+      //         builder: (context, state) => ProfileScreen(),
+      //       ),
+      //       AppRoute(
+      //         name: AppRouteNames.catalog,
+      //         builder: (_, __) => const CatalogScreen(),
+      //         routes: [
+      //           AppRoute(
+      //             name: AppRouteNames.catalogFilters,
+      //             path: AppRouteNames.catalogFilters,
+      //             builder: (_, __) => const FiltersScreen(),
+      //           ),
+      //         ],
+      //       ),
+      //       AppRoute(
+      //         name: AppRouteNames.cart,
+      //         builder: (_, __) => const CartScreen(),
+      //       ),
+      //     ],
+      //   ),
     ],
     // commented until catalog screen is in progress
     redirect: (context, state) {
       if (!authController.isLoggedIn.value) {
-        return (state.subloc == '/login') ? null : '/login';
+        return (state.matchedLocation == '/login') ? null : '/login';
       }
-      return (state.subloc == '/login')
+      return (state.matchedLocation == '/login')
           ? Future.delayed(const Duration(seconds: 3), () {
               return '/home';
             })
